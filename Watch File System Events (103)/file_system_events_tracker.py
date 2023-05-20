aa91755ead7835da790ad80f2e580fc192e93ef6
codervedant <74607608+codervedant@users.noreply.github.com>
2023-05-20 22:30:12 +0530

    Add files via upload

---
+++ Watch File System Events (103)/file_system_events_tracker.py
@@ -0,0 +1,36 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/gicar/Desktop/Downloaded_files"
+
+class FileEventHandler(FileSystemEventHandler):
+
+    def on_created(self, event):
+        print(f"Hey", {event.src_path}, "has been created!")
+
+    def on_deleted(self, event):
+        print(f"Oops! Someone deleted", {event.src_path})
+
+    def on_modified(self, event):
+        print(f"Someone modified", {event.src_path})
+
+    def on_moved(self, event):
+        print(f"Oops! Someone moved", {event.src_path}, "to aanother directory")
+
+event_handler = FileEventHandler()
+observer = Observer()
+observer.schedule(event_handler, from_dir, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("Running...")
+except KeyboardInterrupt:
+    print("Stopped!")
+    observer.stop()
